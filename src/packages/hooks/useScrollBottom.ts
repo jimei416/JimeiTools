99280a1261fad55764022d6bcf2cb320eab95dd4
dujunjie <du_jimei@163.com>
2023-10-16 18:30:06 +0800

    feat(*): useScrollBottom优化

--- src/packages/hooks/useScrollBottom.ts
+++ src/packages/hooks/useScrollBottom.ts
@@ -1,59 +1,69 @@
-import { useEffect, useRef, useState } from "react";
-import {getTargetElement} from '../utils/domTarget';
+import { useEffect, useRef, useState } from 'react';
+import { getTargetElement } from '../utils/domTarget';
 
-const useScrollBottom=(refresh:boolean)=>{
-  const [page,setPage] = useState<number>(0)
-  const target =useRef(null)
-  const flag = useRef(false)
+const useScrollBottom = (
+  refresh: boolean,
+  num: number
+): [
+  React.MutableRefObject<null>,
+  number,
+  React.Dispatch<React.SetStateAction<number>>
+] => {
+  const [page, setPage] = useState<number>(num);
+  const target = useRef(null);
 
-  useEffect(
-    () => {
-      const el = getTargetElement(target, document);
-      if (!el) {
-        return;
-      }
-      const updatePosition = () => {
-        if(!refresh) return 
-        flag.current = false
-        let top:number;
-        let height:number;
-        if (el === document) {
-          if (document.scrollingElement) {
-            top = document.scrollingElement.scrollTop
-            height = document.scrollingElement.scrollHeight - document.scrollingElement.clientHeight
-            if(top===height) flag.current = true
-          } else {
-            top = Math.max(
-              window.pageYOffset,
-              document.documentElement.scrollTop,
-              document.body.scrollTop,
-            )
-            height =Math.max(
-              document.documentElement.scrollHeight - document.documentElement.clientHeight,
-              document.body.scrollHeight - document.body.clientHeight,
-              )            
-            if(top===height) flag.current = true
-          }
+  useEffect(() => {
+    const el = getTargetElement(target, document);
+    if (!el) {
+      return undefined;
+    }
+
+    const updatePosition = () => {
+      if (!refresh) return;
+      let flag = false;
+      let top: number;
+      let height: number;
+      if (el === document) {
+        if (document.scrollingElement) {
+          top = document.scrollingElement.scrollTop;
+          height =
+            document.scrollingElement.scrollHeight -
+            document.scrollingElement.clientHeight;
+          if (top === height) flag = true;
         } else {
-          top = (el as unknown as Element).scrollTop
-          height = (el as unknown as Element).scrollHeight - (el as unknown as Element).clientHeight 
-          if(top===height) flag.current = true
+          top = Math.max(
+            window.pageYOffset,
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+          );
+          height = Math.max(
+            document.documentElement.scrollHeight -
+              document.documentElement.clientHeight,
+            document.body.scrollHeight - document.body.clientHeight
+          );
+          if (top === height) flag = true;
         }
-        if (height === 0 ) flag.current = false
-        if(flag.current){
-          setPage(prev=>prev+1)
-        }
+      } else {
+        top = (el as unknown as Element).scrollTop;
+        height =
+          (el as unknown as Element).scrollHeight -
+          (el as unknown as Element).clientHeight;
+        if (top === height) flag = true;
       }
-      updatePosition();
-      el.addEventListener('scroll', updatePosition);
-      return () => {
-        el.removeEventListener('scroll', updatePosition);
-      };
-    },
-    [target,refresh]
-  );
 
-  return {target,page};
-}
+      if (height === 0) flag = false;
+      if (flag) {
+        setPage((prev) => prev + 1);
+      }
+    };
+    updatePosition();
+    el.addEventListener('scroll', updatePosition);
+    return () => {
+      el.removeEventListener('scroll', updatePosition);
+    };
+  }, [refresh]);
 
-export default useScrollBottom
+  return [target, page, setPage];
+};
+
+export default useScrollBottom;
--- src/pages/ScrollBottom/index.tsx
+++ src/pages/ScrollBottom/index.tsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
 import useScrollBottom from '../../packages/hooks/useScrollBottom'
 const ScrollBottom = ()=>{
-  const [item,setItem] = useState<Array<number>>([])
+  const [item,setItem] = useState<Array<string>>([])
   const [refresh,setRefresh] = useState(true)
-  const {target,page} = useScrollBottom(refresh)
+  const [target,page] = useScrollBottom(refresh,2)
+
   useEffect(()=>{
+    console.log(page);
     if(page>=5) setRefresh(false)
-    setItem(prev=>[...prev,page])
+    setItem(prev=>[...prev,page.toString()])
   },[page])
+  
   return (
     <div className={styles.ScrollBottom} ref={target}>
     {item.map(item=>(
-      <div key={item} className={styles.item}>{item}</div>
+      <div className={styles.item}>{item}</div>
     ))}
   </div>
   )
